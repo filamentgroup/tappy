94924699781744f8104fc989028cc2efa39e281a
Scott Jehl <scottjehl@gmail.com>
2014-03-19 10:22:58 -0500

    use special event api if available, allow X movement to cancel a tap as Y does. Make sure tolerance is used as expected.

--- tappy.js
+++ tappy.js
@@ -9,30 +9,53 @@
 
 			var $el = $( this ),
 				resetTimer,
-				lastScroll,
-				scrollTolerance = 15;
+				startY,
+				startX,
+				cancel,
+				scrollTolerance = 10;
 
 			function trigger( e ){
 				$( e.target ).trigger( "tap", [ e, $( e.target ).attr( "href" ) ] );
 				e.stopImmediatePropagation();
 			}
 
-			function getScroll(){
-				return w.document.body.scrollTop !== undefined ? w.document.body.scrollTop : w.document.documentElement.scrollTop;
+			function getCoords( e ){
+				var ev = e.originalEvent || e,
+					touches = ev.touches || ev.targetTouches;
+
+				if( touches ){
+					return [ touches[ 0 ].pageX, touches[ 0 ].pageY ];
+				}
+				else {
+					return null;
+				}
 			}
 
 			function start( e ){
 				if( e.touches && e.touches.length > 1 || e.targetTouches && e.targetTouches.length > 1 ){
 					return false;
 				}
-				lastScroll = getScroll();
+
+				var coords = getCoords( e );
+				startX = coords[ 0 ];
+				startY = coords[ 1 ];
+			}
+
+			// any touchscroll that results in > tolerance should cancel the tap
+			function move( e ){
+				if( !cancel ){
+					var coords = getCoords( e );
+					if( coords && ( Math.abs( startY - coords[ 1 ] ) > scrollTolerance || Math.abs( startX - coords[ 0 ] ) > scrollTolerance ) ){
+						cancel = true;
+					}
+				}
 			}
 
 			function end( e ){
 				clearTimeout( resetTimer );
 				resetTimer = setTimeout( function(){
 					w.tapHandling = false;
-					lastScroll = null;
+					cancel = false;
 				}, 1000 );
 
 				if( e.ctrlKey || e.metaKey ){
@@ -42,10 +65,11 @@
 				e.preventDefault();
 
 				// this part prevents a double callback from touch and mouse on the same tap
-			
+
 				// if a scroll happened between touchstart and touchend
-				if( w.tapHandling && w.tapHandling !== e.type || ( e.type === "touchend" || e.type === "MSPointerUp" ) && Math.abs( getScroll() - lastScroll ) > scrollTolerance ){
-					return false;
+				if( cancel || w.tapHandling && w.tapHandling !== e.type ){
+					cancel = false;
+					return;
 				}
 
 				w.tapHandling = e.type;
@@ -54,18 +78,32 @@
 
 			$el
 				.bind( "touchstart MSPointerDown", start )
+				.bind( "touchmove MSPointerMove", move )
 				.bind( "touchend MSPointerUp", end )
 				.bind( "click", end );
 		});
 	};
 
-	// monkeybind
-	var oldBind = $.fn.bind;
-	$.fn.bind = function( evt ){
-		if( /(^| )tap( |$)/.test( evt ) ){
-			tap( this );
-		}
-		return oldBind.apply( this, arguments );
-	};
+	// use special events api
+	if( $.event && $.event.special ){
+		$.event.special.tap = {
+			add: function( handleObj ) {
+				tap( $( this ), true );
+			},
+			remove: function( handleObj ) {
+				tap( $( this ), false );
+			}
+		};
+	}
+	else{
+		// monkeybind
+		var oldBind = $.fn.bind;
+		$.fn.bind = function( evt ){
+			if( /(^| )tap( |$)/.test( evt ) ){
+				tap( this );
+			}
+			return oldBind.apply( this, arguments );
+		};
+	}
 
 }( this, jQuery ));
